70b7c3b39f2d8bd86c308792b91e9a2aaa8c632a
Karma Dev <karmaconfigs@gmail.com>
2024-10-22 04:04:56 +0200

    Vite now builds on relative path

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.mjs',
-  },
+	plugins: [react(), tsconfigPaths()],
+	base: './',
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './vitest.setup.mjs',
+	},
 });
